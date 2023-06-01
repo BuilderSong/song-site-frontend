8bdf12c123a5134b5e60df2ef4823fed5ca2e4da
SONG <yuqidkj@gmail.com>
2023-06-01 00:33:26 -0600

    added larger margin x on the single blog page

--- src/pages/BlogSinglePage.js
+++ src/pages/BlogSinglePage.js
@@ -41,7 +41,7 @@ function BlogSinglePage() {
     return (
       <div>
         <Navigation />
-        <div className='flex flex-col justify-center items-center mx-6 md:mx-8 lg:mx-10'>
+        <div className='flex flex-col justify-center items-center mx-20 md:mx-10 lg:mx-12'>
           <h1 className="my-5 md:text-2xl lg:text-3xl">{post.Title}</h1>
 
           <div className='flex justify-start gap-8 mb-4 md:justify-center'>
